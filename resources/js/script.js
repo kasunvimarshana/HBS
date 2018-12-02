0350f020e299f1ef8ace9ee3126fa139ed7b89db
kasunvimarshana <kasunvmail@gmail.com>
2018-12-02 22:25:28 +0530

    init

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,22 @@
-/*$('#index').live('pageshow', function(e, data){    
-    $('#index-content').css('margin-top', ($(window).height() - $('[data-role=header]').height() -       $('[data-role=footer]').height() - $('#index-content').outerHeight()) / 2);
-});
-
-$(window).load(function(){
-	centerContent();
-});
-
-$(window).resize(function(){
-	centerContent();
-});*/
+// function for center element
+function center_element(){
+    $( "#home_content" ).position({
+        my: "center",
+        at: "center",
+        of: "#home"
+    });
+}
 
 $(function(){   //start onload
     
     "use strict";
     
-    $( "#home_content" ).position({
-        my: "center",
-        at: "center",
-        of: "#home"
+    $(window).bind("load", function(){
+        center_element();
+    });
+    
+    $(window).bind("resize", function(){
+        center_element();
     });
     
 }); //end onload
