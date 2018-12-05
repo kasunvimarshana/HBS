36a54011660455dc59baf0e09a77bbdde7516f0e
kasunvimarshana <kasunvmail@gmail.com>
2018-12-05 18:03:03 +0530

    create menu 1 template

---
+++ resources/pages/js/page_menu_1.js
@@ -0,0 +1,14 @@
+$(function(){   //start onload
+    "use strict";
+    
+    /*$.mobile.loadPage( "../resources/results.php", true, {
+        type: "post",
+        data: $( "form#search" ).serialize()
+    });*/
+    
+    /*$("#pagePort").load("pages/index.html", function(){
+        (currentPage.init);
+    });*/
+    
+    
+}); //end onload
