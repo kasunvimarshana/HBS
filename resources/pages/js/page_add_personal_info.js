635cba8b9e304e4d143409d88c526751315494f9
kasunvimarshana <kasunvmail@gmail.com>
2018-12-11 14:45:07 +0530

    change form

--- resources/pages/js/page_add_personal_info.js
+++ resources/pages/js/page_add_personal_info.js
@@ -1,5 +1,10 @@
 $(document).on("pageinit", function(){
     "use strict";
     
-    $("#var_birthday").datepicker();
+    $("#var_birthday").datepicker({
+        inline: true,
+        dateFormat: "yy-mm-dd",
+        changeMonth: true,
+        changeYear: true
+    });
 });
